48cc39d1fe3b1c3494648ac8582726346ce00060
avanchasravani <avanchasravani.16@gmail.com>
2024-11-22 20:33:32 +0530

    Add files via upload

--- Multi123.java
+++ Multi123.java
@@ -1,8 +1,8 @@
 class Multi123
 {
   public static void main(String args[]){
-     int i=5;
-     int j=4;
+     int i=8;
+     int j=10;
      System.out.println("Multiplication of 2 numbers="+(i*j));
   }
 }
